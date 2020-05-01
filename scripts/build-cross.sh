8b3a97520072414576b241b15d09a63b5a0f8644
Berk D. Demir <bdd@mindcast.org>
2020-04-30 18:04:07 -0700

    Add -trimpath to go build flags

--- scripts/build-cross.sh
+++ scripts/build-cross.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 for plat in linux-amd64 linux-arm64 freebsd-amd64; do
   echo "${plat}"
   oa=(${plat/-/ })
-  GOOS="${oa[0]}" GOARCH="${oa[1]}" go build -v -o "build/runitor-${plat}"
+  GOOS="${oa[0]}" GOARCH="${oa[1]}" go build -v -trimpath -o "build/runitor-${plat}"
 done
