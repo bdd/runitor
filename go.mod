343b436237c9082e2c12746e87a029b7102b2ab8
Berk D. Demir <bdd@mindcast.org>
2021-04-11 15:29:13 -0700

    Revamp GH Actions
    - Drop Depaware
    The source of dependencies for this project is the dependency tracker...
    Drop it until we actually depend on something external.
    
    - Prerelease support in upload-release-action
    Tags that contain 'alpha', 'beta', or 'rc' now create prerelease
    releases.
    
    - Stricter tag matching for Release action.
    
    - Simplify Workflow YAMLs by dropping names for obvious steps.

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module bdd.fi/x/runitor
 
 go 1.16
-
-require github.com/tailscale/depaware v0.0.0-20201214215404-77d1e9757027
--- internal/tooldeps/tooldeps.go
+++
@@ -1,7 +0,0 @@
-// Package tooldeps exists solely for depaware tool dependency in go.mod.
-package tooldeps
-
-import (
-	// empty import. (keep the linter happy).
-	_ "github.com/tailscale/depaware/depaware"
-)
