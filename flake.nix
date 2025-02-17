d3ba6898e9825ada65867b887f7835db3b8c2186
Berk D. Demir <bdd@mindcast.org>
2025-02-16 20:28:37 -0800

    Refactor Nix flake
    - Drop flake-utils
    - Update nixpkgs input to unstable for Go 1.24 dependency

--- flake.nix
+++ flake.nix
@@ -3,61 +3,43 @@
 {
   description = "runitor";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    { self, nixpkgs }:
     let
-      pkgs = import nixpkgs { inherit system; };
-      runitor = pkgs.buildGoModule rec {
-        pname = "runitor";
-        revDate = builtins.substring 0 8 (self.lastModifiedDate or "19700101");
-        version = "${revDate}-${self.shortRev or "dirty"}";
-        vendorHash = null;
-        src = ./.;
-        CGO_ENABLED = 0;
-        ldflags = [ "-s" "-w" "-X main.Version=v${version}" ];
-        meta = {
-          homepage = "https://bdd.fi/x/runitor";
-          description = "A command runner with healthchecks.io integration";
-          longDescription = ''
-            Runitor runs the supplied command, captures its output, and based on its exit
-            code reports successful or failed execution to https://healthchecks.io or your
-            private instance.
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
 
-            Healthchecks.io is a web service for monitoring periodic tasks. It's like a
-            dead man's switch for your cron jobs. You get alerted if they don't run on time
-            or terminate with a failure.
-          '';
-          license = pkgs.lib.licenses.bsd0;
-          mainProgram = "runitor";
-          maintainers = [ pkgs.maintainers.bdd ];
-        };
-      };
+      forSupportedSystems =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (system: {
+          default = nixpkgs.legacyPackages.${system}.callPackage f { inherit self; };
+        });
     in
     {
-      devShells = {
-        default = pkgs.mkShell {
-          buildInputs = [
+      devShells = forSupportedSystems (
+        { pkgs, ... }:
+
+        pkgs.mkShell {
+          buildInputs = with pkgs; [
             # build
-            pkgs.go_1_24
+            go_1_24
 
             # release
-            pkgs.gh # create a release on github and upload artifacts
-            pkgs.git # mkrel: git tag, git push
-            pkgs.curl # verify, dlrel, build
-            pkgs.coreutils # sha256sum: sign & verify
-            pkgs.openssh # ssh-keygen: sign & verify
+            gh # create a release on github and upload artifacts
+            git # mkrel: git tag, git push
+            curl # verify, dlrel, build
+            coreutils # sha256sum: sign & verify
+            openssh # ssh-keygen: sign & verify
           ];
-        };
-      };
+        }
+      );
 
-      packages = {
-        inherit runitor;
-        default = runitor;
-      };
-    }
-  );
+      packages = forSupportedSystems ./package.nix;
+    };
 }
---
+++ package.nix
@@ -0,0 +1,31 @@
+{ self, pkgs, ... }:
+
+pkgs.buildGo124Module rec {
+  pname = "runitor";
+  revDate = builtins.substring 0 8 (self.lastModifiedDate or "19700101");
+  version = "${revDate}-${self.shortRev or "dirty"}";
+  vendorHash = "sha256-1atQ9ouvhpc2eOkf+nl1w4OK9xhYVx3ZweeZGcMYPSw=";
+  src = ./.;
+  env.CGO_ENABLED = 0;
+  ldflags = [
+    "-s"
+    "-w"
+    "-X main.Version=v${version}"
+  ];
+  meta = {
+    homepage = "https://bdd.fi/x/runitor";
+    description = "A command runner with healthchecks.io integration";
+    longDescription = ''
+      Runitor runs the supplied command, captures its output, and based on its exit
+      code reports successful or failed execution to https://healthchecks.io or your
+      private instance.
+
+      Healthchecks.io is a web service for monitoring periodic tasks. It's like a
+      dead man's switch for your cron jobs. You get alerted if they don't run on time
+      or terminate with a failure.
+    '';
+    license = pkgs.lib.licenses.bsd0;
+    mainProgram = "runitor";
+    maintainers = [ pkgs.maintainers.bdd ];
+  };
+}
